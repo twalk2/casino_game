5bf60f81eef638ee88dbdeb9f86f7025ad07a713
twalk2 <2twalk2@gmail.com>
2019-09-23 16:04:43 -0600

    added > 0 options menu

--- main.py
+++ main.py
@@ -19,8 +19,19 @@ def wallet_balance():
       print("Please enter a whole dollar amount.\n")
       wallet_balance()
   else:
-    if sum(wallet) <= 0:
+    if sum(wallet) > 0:
       user_input = input("Would you like to continue with, add to, or cashout out your wallet? 1.Continue, 2.Add, 3.Cashout\n")
+      
+      if user_input == "1":
+        user_function()
+
+      elif user_input == "2":
+        add_to_function()
+
+      else:
+        print(f"Here is what you are leaving with: ${sum(wallet)}\n")
+        
+
 
 
 def user_function():
@@ -94,6 +105,23 @@ def balance_display():
   end_price = sum(wallet)
   print(f"Your new balance is ${str(end_price)}\n")
   wallet_balance()
+
+def add_to_function():
+  try:
+      user_input = int(input("How much would you like to add to your wallet?"))
+      
+      if user_input >= 1:
+        wallet.append(user_input)
+        user_function()
+      
+      else:
+        print("Please enter a whole dollar amount.\n")
+        add_to_function()
+    
+  except ValueError:
+    print("Please enter a whole dollar amount.\n")
+    add_to_function()
+
 
 wallet_balance()
 
